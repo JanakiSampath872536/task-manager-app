d85a011e9d10d7c067b759f4e7ebeec47591ee23
Janaki S <145210078+JanakiSampath872536@users.noreply.github.com>
2024-04-20 21:25:13 -0700

    Adding model files

---
+++ models/Task.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  status: {
+    type: String,
+    default: 'Pending',
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
